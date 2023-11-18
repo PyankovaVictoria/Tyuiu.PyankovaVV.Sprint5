989f28eff711edb636aa8414cddefd403198a389
Tyuiu.PyankovaVV@outlook.com <vika.pyankova04@mail.ru>
2023-11-19 01:47:52 +0500

    Add project files.

---
+++ Tyuiu.PyankovaVV.Sprint5.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using Tyuiu.PyankovaVV.Sprint5.Task0.V17.Lib;
+
+namespace Tyuiu.PyankovaVV.Sprint5.Task0.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\Влад\source\repos\Tyuiu.PyankovaVV.Sprint5\Tyuiu.PyankovaVV.Sprint5.Task0.V17\bin\Debug\OutPutFiltTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.PyankovaVV.Sprint5.Task0.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PyankovaVV.Sprint5.Task0.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PyankovaVV.Sprint5.Task0.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc6ec87e-60c0-4c67-894f-53e75947041c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
